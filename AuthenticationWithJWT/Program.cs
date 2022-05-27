524684efcc48de994184dba28bd5825959761ec2
Bruno Sales <baliestri@icloud.com>
2022-05-27 13:43:12 -0300

    add required Dependency Injection

--- AuthenticationWithJWT/Program.cs
+++ AuthenticationWithJWT/Program.cs
@@ -1,7 +1,37 @@
+using System.Text;
+
+using AuthenticationWithJWT.Contracts;
 using AuthenticationWithJWT.EndPoints;
+using AuthenticationWithJWT.Repositories;
+using AuthenticationWithJWT.Services;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+var configuration = builder.Configuration;
+
+services
+  .AddScoped<IUserRepository, UserRepository>()
+  .AddScoped<ITokenService, TokenService>();
+
+services
+  .AddAuthorization()
+  .AddAuthentication(x => {
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  })
+  .AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters {
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["tokenSecret"])),
+      ValidateIssuer = false,
+      ValidateAudience = false
+    };
+  });
 
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
@@ -13,8 +43,14 @@ if (app.Environment.IsDevelopment()) {
   app.UseSwaggerUI();
 }
 
-app.MapWeatherForecastsEndPoints();
+app
+  .MapAuthEndPoints()
+  .MapWeatherForecastsEndPoints();
 
 app.UseHttpsRedirection();
+
+app
+  .UseAuthentication()
+  .UseAuthorization();
 
 app.Run();
