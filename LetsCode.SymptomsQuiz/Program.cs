20827f3e9bc0c851a9a5fda38ca37df6fe9e18a9
Bruno Sales <baliestri@icloud.com>
2022-05-27 19:22:56 -0300

    finished SymptomsQuiz project

--- LetsCode.SymptomsQuiz/Program.cs
+++ LetsCode.SymptomsQuiz/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var results = new Dictionary<string, string> {
+  { "SSNNS", "Dengue" },
+  { "SSSSN", "Gripe" },
+  { "NSSSN", "Gripe" },
+  { "SNNNN", "Sem doenças" },
+  { "NNNNN", "Sem doenças" },
+};
+
+char GetConfirm(string message) {
+  Console.WriteLine(message);
+  Console.Write("[s]im ou [n]ão (padrão: não): ");
+
+  var key = Console.ReadKey().Key;
+
+  var answer = key switch {
+    ConsoleKey.Y or ConsoleKey.S => 'S',
+    ConsoleKey.N => 'N',
+    _ => 'N'
+  };
+
+  Console.WriteLine();
+
+  return answer;
+}
+
+var questions = new[] {
+  GetConfirm("Sente dor no corpo?"),
+  GetConfirm("Você tem febre?"),
+  GetConfirm("Você tem tosse?"),
+  GetConfirm("Está com congestão nasal?"),
+  GetConfirm("Tem manchas pelo corpo?")
+};
+
+Console.WriteLine($"Você tem: {results[string.Join("", questions)]}");
